ae9bb75fe515ad4840a035336eed7e601aa00093
jule1208 <152274842+jule1208@users.noreply.github.com>
2024-03-20 11:09:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,5 +13,29 @@ function executeExample() {
     document.body.appendChild(canvas);
 }
 
+const beispiel2 = document.querySelector("#beispiel2");
+beispiel2.addEventListener("click", executeExample2);
+
+function executeExample2() {
+
+    let c = document.getElementById("myCanvas");
+    let ctx = c.getContext("2d");
+
+    ctx.fillStyle = "lightblue";
+    ctx.fillRect(20, 20, 75, 50);
+    ctx.globalAlpha = 0.2;
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(50, 50, 75, 50);
+
+    ctx.fillStyle = "darkblue";
+    ctx.fillRect(80, 80, 75, 50);
+
+    document.body.appendChild(myCanvas);
+}
+
+
+
+
 
 
